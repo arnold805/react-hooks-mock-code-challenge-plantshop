be4b4897fa177ddd74885718de4314c9db1d7c5d
Greg <garnold.805@gmail.com>
2022-05-05 10:33:17 -0400

    Completed Assignment

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plants}) {
+
+  const plantsList = plants.map((p) =>
+    <PlantCard
+    key={p.id}
+    name={p.name}
+    image={p.image}
+    price={p.price}
+    />
+  )
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plantsList}</ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants/")
+    .then (r => r.json())
+    .then(setPlants);
+  }, []);
+
+  const filteredPlants = plants.filter((plant) => {
+    return plant.name.toLowerCase().includes(search.toLowerCase());
+  });
+
   return (
     <main>
       <NewPlantForm />
-      <Search />
-      <PlantList />
+      <Search search={search} setSearch={setSearch}/>
+      <PlantList plants={filteredPlants}/>
     </main>
   );
 }
